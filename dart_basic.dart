63d84ef96c1f751a1ca862d9fefb5fce01529e37
Shahzaib <shahzaibrizwan7@gmail.com>
2022-10-09 17:15:46 +0500

    dart_basic

---
+++ dart_basic.dart
@@ -0,0 +1,21 @@
+void main(){
+  String c = 'Shahzaib';
+  String b = 'Rizwan';
+  print('My name is $c');
+  print('My father name is ${c.length}');
+  // var
+  var a;
+  a=10;
+  print(a);
+  a='shahza';
+  print(a.runtimeType);
+  
+  // if else statment
+  int z=4;
+  if(z==4){
+  print("True");
+  }
+  else{
+    print('false');
+  }
+}
