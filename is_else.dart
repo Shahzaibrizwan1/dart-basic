1519fd955ae43c2f36ca7ecb039371fc2d3906eb
Shahzaib <shahzaibrizwan7@gmail.com>
2022-10-09 20:13:43 +0500

    if_else

---
+++ is_else.dart
@@ -0,0 +1,18 @@
+void main(){
+    int a=25;
+    if(a<10){
+        print("less than 10");
+    }
+    else if(a<20){
+        print("less than 20");
+    }
+     else if(a<30){
+        print("less than 30");
+    }
+     else if(a<40){
+        print("less than 40");
+    }
+    else{
+        print("not valid");
+    }
+}
