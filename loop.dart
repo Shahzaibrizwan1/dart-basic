37109c56cbbe32680d987317210dc4ce824f679b
Shahzaib <shahzaibrizwan7@gmail.com>
2022-10-09 21:19:09 +0500

    loop

---
+++ loop.dart
@@ -0,0 +1,23 @@
+//for loop
+
+void main(){
+    for (var i = 0; i<5; i++){
+      print(i);
+}
+
+// while loop
+
+var i =100;
+  while(i<100){
+    print("while hello");
+    i++;
+  }
+
+//do while loop
+
+  var j = 200;
+  do{
+    print("do while hello");
+    i++;
+  }while(j<200);
+}
