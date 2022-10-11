65eccf682997298a5aa882759bae111e1fcd24e3
Shahzaib <shahzaibrizwan7@gmail.com>
2022-10-11 20:17:02 +0500

    function_instance_object

--- class_object.dart
+++ class_object.dart
@@ -1,15 +1,21 @@
 void main(){
-var obj = Student ();
-  obj.display();
+// Creating an Object
+  var sumsung = new Mobile();
+
+// Calling instance method using object
+  sumsung.showModel("A 100");
+}
+class Mobile{
+//instance variable
+
+  String ? model;
+
+// instance method
+
+  
+showModel(md){
+  model = md;
+  print(model);
 }
   
-class Student {
-  //instance variable 
-  var name = 'shahzaib';
-  var age = '25';
-  //instance method
-  void display(){
-    print('Name = $name');
-    print('age = $age');
-  }
-}  
+}
